b13f7b31619d0a9c7870fa7dafa352a24045c5bf
SerAyaKz <ayanbek.as@gmail.com>
2023-07-01 18:59:07 +0600

    first commit

---
+++ src/main/java/kz/com/SerAya/SerAyaApplication.java
@@ -0,0 +1,13 @@
+package kz.com.SerAya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerAyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerAyaApplication.class, args);
+	}
+
+}
